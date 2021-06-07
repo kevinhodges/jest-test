154dbcce71e36745e9150cc318564ee39e4ad1a0
Kevin Hodges <kevin.hodges@c02f92tpmd6n.lan>
2021-06-07 16:14:29 +0100

    tweak test names

--- test/helper.js
+++ test/helper.js
@@ -7,13 +7,13 @@ module.exports = {
     console.log('testData', testData)
 
     describe('a and b = 3', () => {
-      it('adds a and b to equal 3', () => {
+      it('does something to a and b and equal 3', () => {
         expect(objectToTest(testData['test1'][0], testData['test1'][1])).toBe(3);
       });
     })
 
     describe('a and b = 5', () => {
-      it('adds a and b to equal 5', () => {
+      it('does something to a and b and equal 5', () => {
         expect(objectToTest(testData['test2'][0], testData['test2'][1])).toBe(5);
       });
     })
